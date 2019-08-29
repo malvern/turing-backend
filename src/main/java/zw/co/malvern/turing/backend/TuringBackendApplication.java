a7fb4b8aa0b900cab0e6776febf2f35b00acbc6f
malverndongeni <malvern.dongeni@econet.co.zw>
2019-08-29 12:27:55 +0200

    Initial commit

---
+++ src/main/java/zw/co/malvern/turing/backend/TuringBackendApplication.java
@@ -0,0 +1,13 @@
+package zw.co.malvern.turing.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuringBackendApplication.class, args);
+	}
+
+}
